d62a4fe950d8f464b8695568d3c8408b105820d0
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-12-21 11:42:28 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+what_and_who = {'key0': [],}
+inf = {'key0':['name','surname','email']}
+
+def key_by_num(key, what):
+    for i in what_and_who:
+        if i == key:
+            what_and_who[i].append(what)
+
+    print(what_and_who)
+def reg(key,name,surname,email):
+    inf[key] = []
+    inf[key].append(name)
+    inf[key].append(surname)
+    inf[key].append(email)
+    what_and_who[key] = []
+
+
+
